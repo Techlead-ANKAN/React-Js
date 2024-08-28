946f3018551c7a85337ba48204ba8cb51835c2aa
ANKAN MAITY <mr.ankanmaity@gmail.com>
2024-08-28 21:22:27 +0530

    Did the same thing with 01basicreact and understand the how react works.

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,13 @@
+import Myfunc from "./myfile"
+
 
 // This is the App which returns a HTML 
 function App() {
   return (
+    <>
     <h1>Basic React App</h1>
+    <Myfunc />
+    </>
   );
 }
 
---
+++ 01basicreact/src/myfile.js
@@ -0,0 +1,7 @@
+function Myfunc() {
+    return (
+        <h2>This is myfile.js</h2>
+    )
+}
+
+export default Myfunc;
