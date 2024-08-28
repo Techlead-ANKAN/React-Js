0196c50d8b2c1f0c00079ad409244d7a4650eda4
ANKAN MAITY <mr.ankanmaity@gmail.com>
2024-08-28 18:45:27 +0530

    here i have learnt: (i) how to create a basic react app. (ii) how to create a react app through vite bundler. (iii) Manipulated (deleted) some files that are not required or that i cannot understand. (iv) Created a NOTES.txt for writing what some essentials that i have learned

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Basic React App</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React App with Vite</h1>
+  );
+}
+
+export default App
