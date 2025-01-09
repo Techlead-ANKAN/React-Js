340c6d6df6306d5ee848da4cd8f25353fba70f87
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-01-09 23:29:27 +0530

    Completed the only creating the components file

--- 11_TodDo_Context_LocalStorage/src/App.jsx
+++ 11_TodDo_Context_LocalStorage/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ToDoProvider from "./context"
 import './App.css'
 
@@ -21,10 +21,26 @@ function App() {
     setTodos((prev) => prev.filter((todo) => todo.id !== id ))
   }
 
-  // 4) Functionality to "toggle" completionof todos
+  // 4) Functionality to "toggle" completion of todos
   const toggleComplete = (id) => {
     setTodos((prev) => prev.map((prevTodo) => {prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo}))
   }
+
+  // Local Storage Feature
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem('todos'));
+
+    if(todos && todos.length > 0){
+      setTodos(todos);
+    }
+  }, [])
+
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+  
 
   return (
   
