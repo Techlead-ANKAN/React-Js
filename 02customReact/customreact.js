77fee353f86fcae482f5c0884b4869fc7b9bbd54
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-01-17 10:56:57 +0530

    Recap of react Topics and updating some works

--- 02customReact/customreact.js
+++ 02customReact/customreact.js
@@ -1,19 +1,5 @@
 function customRender(reactElement, container) {
 
-
-    // Version - 1 (This version is ok but incase of the no.of attributes it could be a problem so we need to generalize it.) 
-    /*  
-    const domEle = document.createElement(reactElement.type);
-    
-    domEle.setAttribute("href", reactElement.props.href);
-    domEle.setAttribute("target", reactElement.props.target);
-
-    domEle.innerHTML = reactElement.children;
-
-    container.appendChild(domEle);
-    */
-
-    // Version - 2
     const domEle = document.createElement(reactElement.type);
 
     domEle.innerHTML = reactElement.children;
