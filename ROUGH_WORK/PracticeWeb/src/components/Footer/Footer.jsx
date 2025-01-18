13e05997d76b730ca59055b75d81b8ac67d605fd
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-01-18 13:00:00 +0530

    Completed the basic footer section

--- ROUGH_WORK/PracticeWeb/src/components/Footer/Footer.jsx
+++ ROUGH_WORK/PracticeWeb/src/components/Footer/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./footer.css"
 import { Link, NavLink } from 'react-router-dom';
 
 export default function Footer() {
   return (
-      <></>
+
+      <footer>
+        <div className="footer">
+          This is Footer Section
+        </div>
+      </footer>
+
   );
 }
--- ROUGH_WORK/PracticeWeb/src/components/Home/Home.jsx
+++ ROUGH_WORK/PracticeWeb/src/components/Home/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Home() {
   return (
-    <></>
+    <>
+    Home</>
   )
 }
 
