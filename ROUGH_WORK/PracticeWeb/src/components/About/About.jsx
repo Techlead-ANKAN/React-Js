f5d5296f1354174fb24f0123d34887dfda956535
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-01-18 13:14:15 +0530

    Completed the About Page

--- ROUGH_WORK/PracticeWeb/src/components/About/About.jsx
+++ ROUGH_WORK/PracticeWeb/src/components/About/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import "./about.css"
 
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <iframe src="https://tailwindcss.com/docs/guides/create-react-app" frameborder="0"></iframe>
+    </div>
   )
 }
 
--- ROUGH_WORK/PracticeWeb/src/components/Home/Home.jsx
+++ ROUGH_WORK/PracticeWeb/src/components/Home/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function Home() {
   return (
     <>
-    Home</>
+    Home
+    </>
   )
 }
 
