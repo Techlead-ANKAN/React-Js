f174e822c0f6f51dba09b93307d8dd8c304cb084
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-01-27 22:59:50 +0530

    created the authSlice.js

--- 13_MegaBlog/src/store/authSlice.js
+++ 13_MegaBlog/src/store/authSlice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+// initial state of the authentication slice
 const initialState = {
     status: false, // whether the user is authenticated or not
     userData: null
@@ -7,13 +9,19 @@ const initialState = {
 
 // this slice is to find out that whether user is authenticated or not. we will ask these from the store everytime
 const authSlice = createSlice({
-    name: "auth",
+    name: "auth", // name of the slice
     initialState,
+
+    // an object containing reducers of the slice(authSlice)
     reducers: {
+
+        // reducers are defined here
+
         login: (state, action) => {
             state.status = true;
             state.userData = action.payload.userData;
         }, 
+        
         logout: (state) => {
             state.status = false;
             state.userData = null;
