0032299b338b5aa2524840bc98a32d2a3549a6b6
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-02-01 10:39:26 +0530

    Created the store and the authSlice.js with login and logout as the reducers for authentication.

---
+++ ROUGH_WORK/Postsbook/src/store/authSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    userStatus: false,
+    userData: null
+}
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState,
+    reducres: {
+        login: (state, action) => {
+            state.userStatus = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.userStatus = false;
+            state.userData = null;
+        }    
+    }
+});
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducers;
