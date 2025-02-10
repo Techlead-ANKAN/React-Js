b8e489b0d1a777832e4349a5aa983603fb2c8cc3
ANKAN MAITY <mr.ankanmaity@gmail.com>
2025-02-11 00:45:06 +0530

    created the Signup.jsx using the React Form hook

---
+++ 13_MegaBlog/src/components/Signup.jsx
@@ -0,0 +1,80 @@
+import React, {useState} from 'react';
+import { AuthService } from '../appwrite/auth';
+import {Link, useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {useForm} from "react-hook-form";
+import { login } from '../store/authSlice';
+import {Button, Input, Logo} from "./index"
+
+function Signup() {
+
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
+    const dispatch = useDispatch();
+    const{register, handleSubmit} = useForm();
+
+    const signup = async(data) => {
+        setError("");
+        try{
+            const userData = await AuthService.createAccount(data);
+
+            if(userData){
+                const userData = await AuthService.getCurrentUser();
+
+                if(userData){
+                    dispatch(login(userData));
+                    navigate("/");
+                }
+            }
+        }catch(error){
+            setError(error.nessage);
+        }
+    }
+
+  return (
+<div className="flex items-center justify-center">
+            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+            <div className="mb-2 flex justify-center">
+                    <span className="inline-block w-full max-w-[100px]">
+                        <Logo width="100%"/>
+                    </span>
+            </div>
+                <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
+                <p className="mt-2 text-center text-base text-black/60">
+                    Already have an account?&nbsp;
+                    
+                </p>
+                {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
+
+                <form onSubmit={handleSubmit(signup)}>
+                    <div className='space-y-5'>
+                        <Input 
+                            label="Full Name: "
+                            placeholder="Enter your full name"
+                            {...register("Name", {
+                                required: true
+                            })}
+                        />
+
+                        <Input label="Email: " placeholder="Enter your email..." type="email" {...register("email", {required: true, validate: {matchPattern: (value) => {/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                                "Email address must be a valid address"}}})}  />
+
+                        <Input 
+                        label="Password: "
+                        type="password"
+                        placeholder="Enter your password"
+                        {...register("password",{
+                            required: true
+                        })}
+                        />
+
+                        <Button type='submit' className='w-full'>Create Account</Button>
+                    </div>
+                </form>
+            </div>
+
+    </div>
+  )
+}
+
+export default Signup
